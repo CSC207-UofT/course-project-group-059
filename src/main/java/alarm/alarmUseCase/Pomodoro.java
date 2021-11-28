ee5de4200430c742909df3b557b0b402ef7adce5
soggytoast321 <jtan3160@gmail.com>
2021-11-28 00:18:36 -0500

    Fixed Pomodoro hard coding and updated tests

--- src/main/java/alarm/alarmUseCase/Pomodoro.java
+++ src/main/java/alarm/alarmUseCase/Pomodoro.java
@@ -3,12 +3,24 @@ package alarm.alarmUseCase;
 import alarm.alarmClockEntities.AlarmClock;
 
 public class Pomodoro {
+    private final static String workMsg = "Start working now!";
+    private final static String breakMsg = "Start relaxing now!";
+
     private int pomodoroCount;
     private AlarmClock alarm;
 
+    private final int finalIndex; //index of last interval in cycle (long break)
+    private final int shortBreakDuration, longBreakDuration, workDuration;
+
     //constructor
-    public Pomodoro() {
+    public Pomodoro(int workIntervals, int shortDur, int longDur, int workDur) {
         this.pomodoroCount = 0;
+
+        this.finalIndex = workIntervals *2 - 1;
+
+        this.shortBreakDuration = shortDur;
+        this.longBreakDuration = longDur;
+        this.workDuration = workDur;
         startWorkAlarm();
     }
 
@@ -16,40 +28,47 @@ public class Pomodoro {
     private void updateCount() {
         this.pomodoroCount++;
 
-        if (pomodoroCount > 7) { //reset count
+        if (pomodoroCount > this.finalIndex) { //reset count
             this.pomodoroCount = 0;
         }
     }
 
     //starts alarm for working and returns a message
     private String startWorkAlarm() {
-        this.alarm = new AlarmClock(25);
-        return "Start working now!";
+        this.alarm = new AlarmClock(this.workDuration);
+        return workMsg;
     }
 
     //starts alarm for relaxing and returns a message
     private String startRelaxAlarm(int dur) {
         this.alarm = new AlarmClock(dur);
-        return "Start relaxing now!";
+        return breakMsg;
     }
 
     //run this every minute
     public String update() {
         String message = "";
         if (this.alarm.sound()) {
-            //pomodoro count - work: 0, 2, 4, 6, short break: 1, 3, 5, long break: 7
             updateCount();
 
             if (this.pomodoroCount%2 == 0) {
                 message = startWorkAlarm();
             }
-            else if (this.pomodoroCount == 7) {
-                message = startRelaxAlarm(15); //take long break
+            else if (this.pomodoroCount == this.finalIndex) { //last interval in cycle
+                message = startRelaxAlarm(this.longBreakDuration); //take long break
             }
             else {
-                message = startRelaxAlarm(5); //take short break
+                message = startRelaxAlarm(this.shortBreakDuration); //take short break
             }
         }
         return message;
+    }
+
+    public static String getWorkMsg() {
+        return workMsg;
+    }
+
+    public static String getBreakMsg() {
+        return breakMsg;
     }
 }
