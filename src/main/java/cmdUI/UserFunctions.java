bf385a0208918ede1be30dab3bb291af5e78a502
maxthewang <maxthe.wang@mail.utoronto.ca>
2021-12-02 05:17:01 -0500

    Extended UserFunctions

--- src/main/java/cmdUI/UserFunctions.java
+++ src/main/java/cmdUI/UserFunctions.java
@@ -1,5 +1,7 @@
 package cmdUI;
 
+import alarm.alarmUseCase.Pomodoro;
+import gateways.CSVManager;
 import printers.TaskListPrinter;
 import printers.TimelinePrinter;
 
@@ -23,15 +25,21 @@ public class UserFunctions{
         this.tracker = new TaskTracker(this.tasklist);
         this.timeline = new Timeline();
         this.timelineManager = new TimelineManager(this.timeline);
+
+        // Load saved CSV file if it exists
+        loadCSV();
+
+        System.out.println("Welcome back!");
+        System.out.println("What would you like to do?");
     }
 
     // Begin prompt which allows user to access features
     public void mainMenu() {
-        System.out.println("Welcome back!");
-        System.out.println("What would you like to do?");
-        String msg = "\nM to manage tasks" +
-                "\nP to print information" +
-                "\nX to exit";
+        String msg = "\n1: Manage tasks" +
+                "\n2: Print information" +
+                "\n3: Start Pomodoro timer" +
+                "\n4: Save and exit program" +
+                "\nType the corresponding number: ";
 
         // Repeat message until user chooses to exit
         while (true) {
@@ -40,22 +48,40 @@ public class UserFunctions{
             Scanner reader = new Scanner(System.in);
             String key = reader.next();
 
-            if (key.equalsIgnoreCase("M")) {
+            if (key.equalsIgnoreCase("1")) {
                 manageTask();
             }
-            else if (key.equalsIgnoreCase("P")) {
+            else if (key.equalsIgnoreCase("2")) {
                 printer();
             }
-            else if (key.equalsIgnoreCase("X")) {
+            else if (key.equalsIgnoreCase("3")) {
+                startPomodoro();
+            }
+            // Save state to CSV, then break loop
+            else {
+                System.out.println("Saving data...");
+                saveCSV();
+                System.out.println("Exiting program.");
                 break;
             }
         }
     }
 
+    public void loadCSV() {
+        // TODO: Implement when CSVManager is completed
+        // CSVManager.load();
+    }
+
+    public void saveCSV() {
+        // TODO: Implement when CSVManager is completed
+        // CSVManager.save(this.tracker, this.timelineManager);
+    }
+
     // Ask user what to do, and access the corresponding method in the TaskTracker
     public void manageTask(){
-        String msg = "\nA to create and add a task" +
-                "\nENTER to exit";
+        String msg = "\n1: Create and add a task" +
+                "\n2: Edit tasks" +
+                "\n3: Return to main menu";
 
         System.out.println(msg);
 
@@ -63,17 +89,21 @@ public class UserFunctions{
         String key = reader.nextLine();
 
         // Call createTask
-        if (key.equalsIgnoreCase("A")) {
+        if (key.equalsIgnoreCase("1")) {
             TaskCreator.createTask(this.tracker);
+        }
+        else if (key.equalsIgnoreCase("2")) {
+            // TODO: Implement when task editing is completed
+            System.out.println("Not implemented yet");
         }
     }
 
     // Print either Timeline, TaskList or Suggestion list
     public void printer(){
-        String msg = "\nM to show timeline" +
-                "\nL to show to-do list" +
-                "\nS to show suggestion list" +
-                "\nENTER to exit";
+        String msg = "\n1: Show timeline" +
+                "\n2: Show to-do list" +
+                "\n3: Show suggestion list" +
+                "\n4: Return to main menu";
 
         System.out.println(msg);
 
@@ -81,19 +111,25 @@ public class UserFunctions{
         String key = reader.nextLine();
 
         // Call print method in respective printer classes
-        if (key.equalsIgnoreCase("M")) {
+        if (key.equalsIgnoreCase("1")) {
             TimelinePrinter.print(this.timeline);
         }
-        else if (key.equalsIgnoreCase("L")) {
+        else if (key.equalsIgnoreCase("2")) {
             TaskListPrinter.print(this.tracker);
         }
-        else if (key.equalsIgnoreCase("S")) {
+        else if (key.equalsIgnoreCase("3")) {
+            // TODO: Implement when suggestions are completed
             System.out.println("Not implemented yet");
         }
     }
 
-    // TODO: Add method for Pomodoro
-
-
-
+    // Creates and starts an instance of Pomodoro
+    public void startPomodoro(){
+        int workIntervals = 2;
+        int shortDur = 5;
+        int longDur = 20;
+        int workDur = 25;
+        Pomodoro pomodoro = new Pomodoro(workIntervals, shortDur, longDur, workDur);
+        System.out.println("Pomodoro started.");
+    }
 }
