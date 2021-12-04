38fcde92207d7b82f0861a551be5e2c55c4d54df
soggytoast321 <jtan3160@gmail.com>
2021-12-03 23:02:05 -0500

    Skeleton of Storable and CSVManager for feedback purposes. There are comments in Storable and CSVManager.

--- src/main/java/storer/Storable.java
+++ src/main/java/storer/Storable.java
@@ -3,6 +3,6 @@ package storer;
 import java.util.ArrayList;
 
 public interface Storable {
-    ArrayList<String> save(); //writes the list into .csv format with each element being a line in the file
-    void open(ArrayList<String> csv); //reads and gets data from csv, then replaces current object with new data
+    ArrayList<String> save(); //writes the list object into .csv format with each element being a line in the file
+    void open(ArrayList<String> csv); //reads and gets new object data from csv, then overrides current object
 }
